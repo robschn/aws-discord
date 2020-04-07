417ea5955562e5ed4d642595d96600e90ba0b0b9
Robert Schneider <robmschn@gmail.com>
2020-04-06 22:11:35 -0400

    added commands to status_instance

--- bot.py
+++ bot.py
@@ -18,10 +18,6 @@ bot = commands.Bot(command_prefix='!')
 async def start_instance(ctx):
     mention = ctx.author.id
     ec2.start_instances(InstanceIds=[instance])
-    instance_info = ec2.describe_instances()
-    if instance_info['Reservations']['Instances']['InstanceId'] == instance:
-        instance_ip = instance_info['Reservations']['Instances']['PublicIpAddress']
-
     await ctx.send(f'<@{mention}> Starting Minecraft server..')
 
 @bot.command(name='stop', help='Stops Minecraft Server')
@@ -32,9 +28,10 @@ async def stop_instance(ctx):
 
 @bot.command(name='status', help='Displays the status of the Minecraft server')
 async def status_instance(ctx):
-    # todo add in status
-    # mention = ctx.author.id
-    # ec2.stop_instances(InstanceIds=[instance])
-    # await ctx.send(f'<@{mention}> Stopping Minecraft server..')
+    mention = ctx.author.id
+    instance_info = ec2.describe_instances()
+    if instance_info['Reservations']['Instances']['InstanceId'] == instance:
+        instance_ip = instance_info['Reservations']['Instances']['PublicIpAddress']
+    await ctx.send(f'<@{mention}> Minecraft server IP: {instance_ip}}')
 
 bot.run(token)
