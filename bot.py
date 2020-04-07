504973539a67e7fa3099e11951cc0cede3c621f4
Robert Schneider <robmschn@gmail.com>
2020-04-06 21:42:44 -0400

    added commands

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+import pandas as pd
+from discord.ext import commands
+from dotenv import load_dotenv
+
+import botfunctions
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='start', help='Starts Minecraft server')
+async def purchase_price(ctx, purchase_price: int):
+    mention = ctx.author.id
+    await ctx.send(f'<@{mention}> Starting instance..')
+
+@bot.command(name='stop', help='Starts Minecraft Server')
+async def purchase_price(ctx, purchase_price: int):
+    mention = ctx.author.id
+    await ctx.send(f'<@{mention}> Stopping instance..')
+
+bot.run(TOKEN)
