5f42a098d66e30998e0959566427cf7755262cbc
Robert Schneider <robmschn@gmail.com>
2020-04-06 21:54:44 -0400

    added commands

--- bot.py
+++ bot.py
@@ -1,25 +1,38 @@
 import os
 import time
 
+import boto3
 from discord.ext import commands
 from dotenv import load_dotenv
 
-import botfunctions
-
-
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+
+token = os.getenv('DISCORD_TOKEN')
+
+region = os.getenv('REGION_ID')
+instance = os.getenv('INSTANCE_ID')
+
+ec2 = boto3.client('ec2', region_name=region)
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='start', help='Starts Minecraft server')
-async def purchase_price(ctx, purchase_price: int):
+async def start_instance(ctx):
     mention = ctx.author.id
-    await ctx.send(f'<@{mention}> Starting instance..')
+    ec2.start_instances(InstanceIds=[instance])
+    await ctx.send(f'<@{mention}> Starting Minecraft server..')
 
-@bot.command(name='stop', help='Starts Minecraft Server')
-async def purchase_price(ctx, purchase_price: int):
+@bot.command(name='stop', help='Stops Minecraft Server')
+async def stop_instance(ctx):
     mention = ctx.author.id
-    await ctx.send(f'<@{mention}> Stopping instance..')
+    ec2.stop_instances(InstanceIds=[instance])
+    await ctx.send(f'<@{mention}> Stopping Minecraft server..')
 
-bot.run(TOKEN)
+@bot.command(name='status', help='Displays the status of the Minecraft server')
+async def status_instance(ctx):
+    # todo add in status
+    # mention = ctx.author.id
+    # ec2.stop_instances(InstanceIds=[instance])
+    # await ctx.send(f'<@{mention}> Stopping Minecraft server..')
+
+bot.run(token)
